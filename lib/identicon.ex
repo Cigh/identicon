223f12ef3abc4340d9f598403acb5a7ce2038b57
LetVogel <let.vogel@protonmail.com>
2018-09-02 20:21:20 +0200

    Build pixel_map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,21 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_value, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     filtered_grid = 
       Enum.filter grid, fn({value, _index}) ->
